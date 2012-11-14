50a4dc3f243bff36aa41598af06f74001610d612
hm-dev <huangming@umeng.com>
2012-11-14 22:22:15 +0800

    改缩进

--- code/connection.cpp
+++ code/connection.cpp
@@ -41,33 +41,33 @@ void connection::handle_read(const boost::system::error_code& e, std::size_t byt
 		boost::tribool result;
 		boost::tie(result, boost::tuples::ignore) = request_parser_.parse(request_, buffer_.data(), buffer_.data() + bytes_transferred);
 
-    if (result)
-    {
-		request_handler_.handle_request(request_, reply_);
-		boost::asio::async_write(socket_, reply_.to_buffers(),
-			strand_.wrap(boost::bind(&connection::handle_write, shared_from_this(),
-				boost::asio::placeholders::error)));
-    }
-    else if (!result)
-    {
-		reply_ = reply::stock_reply(reply::bad_request);
-		boost::asio::async_write(socket_, reply_.to_buffers(),
-			strand_.wrap(boost::bind(&connection::handle_write, shared_from_this(),
-				boost::asio::placeholders::error)));
-    }
-    else
-    {
-		socket_.async_read_some(boost::asio::buffer(buffer_),
-			strand_.wrap(boost::bind(&connection::handle_read, shared_from_this(),
-				boost::asio::placeholders::error,
-				boost::asio::placeholders::bytes_transferred)));
-    }
-  }
+		if (result)
+		{
+			request_handler_.handle_request(request_, reply_);
+			boost::asio::async_write(socket_, reply_.to_buffers(),
+				strand_.wrap(boost::bind(&connection::handle_write, shared_from_this(),
+					boost::asio::placeholders::error)));
+		}
+		else if (!result)
+		{
+			reply_ = reply::stock_reply(reply::bad_request);
+			boost::asio::async_write(socket_, reply_.to_buffers(),
+				strand_.wrap(boost::bind(&connection::handle_write, shared_from_this(),
+					boost::asio::placeholders::error)));
+		}
+		else
+		{
+			socket_.async_read_some(boost::asio::buffer(buffer_),
+				strand_.wrap(boost::bind(&connection::handle_read, shared_from_this(),
+					boost::asio::placeholders::error,
+					boost::asio::placeholders::bytes_transferred)));
+		}
+	}
 
-  // If an error occurs then no new asynchronous operations are started. This
-  // means that all shared_ptr references to the connection object will
-  // disappear and the object will be destroyed automatically after this
-  // handler returns. The connection class's destructor closes the socket.
+	// If an error occurs then no new asynchronous operations are started. This
+	// means that all shared_ptr references to the connection object will
+	// disappear and the object will be destroyed automatically after this
+	// handler returns. The connection class's destructor closes the socket.
 }
 
 void connection::handle_write(const boost::system::error_code& e)
